4bb81f5a513389a5fe5a61ee34e637adafa17d6b
joakim-klemetsen <joakim.klemetsen@icloud.com>
2023-10-04 18:44:14 +0200

    Updating the code
    Thought I had a solution, but found out that the all the variables in the data frame was of type 'chr'. Therefore, I turned to using the code in 'galaxy_skeleton2.R'

--- problem2.R
+++ problem2.R
@@ -1,40 +1,48 @@
 # Problem 2 - Git and Github Assignment
 
-# Loading required packages ---
+# Load required packages ---
 library(tidyverse)
 # ---
 
-# Loading in the raw data ---
-raw_data <- readLines("suites_dw_Table1.txt")
+# Load raw data ---
+raw_file <- readLines("suites_dw_Table1.txt")
 # ---
 
-# Storing the variable descriptions ---
+# Saving the variable descriptions ---
 
-# Find the line where data headers start
-# 'name' appears two times, we are interested in the second
-header_start <- which(grepl("^\\s*name\\s*", raw_data))[2] # <- 2.
+# Identify the header line
+header_start <- which(grepl("^\\s*name\\s*", raw_file))[2]
 
-# Directly extract all lines before the header
-var_descr <- raw_data[1:(header_start-2)]
+# Save variable descriptions
+cat(raw_file[1:(header_start-2)], 
+    sep = "\n", 
+    file = "variable_descriptions.txt")
 # ---
 
-# Formatting the data frame ---
+# Read data to a tidy data frame ---
 
-# Initial formatting
+# Read the data into a tibble, skipping the lines stored in 'var_descr'
 data <- 
   read_delim(
-    "suites_dw_Table1.txt", # using the table data
-    delim = "|",            # character used to separate fields
-    skip = header_start-2,  # skip importing of variable descriptions
-    trim_ws = TRUE          # trimming white spaces   
+    "suites_dw_Table1.txt", 
+    delim = "|",           
+    skip = header_start-2, 
+    trim_ws = TRUE          
   ) %>% 
+  # Clean names and trim white spaces
   janitor::clean_names() %>% 
   mutate(across(everything(), str_trim)) %>% 
+  # Convert empty strings to NA
   mutate(across(where(is.character), na_if,"")) %>% 
   mutate(across(where(is.numeric), na_if, NA))
 
-# Removing the annoying variable separator from the table
+# Remove annoying separator line from the data frame
 data <- 
   data %>% 
   slice(-1)
 # ---
+
+df_test <- read_csv(
+  "suites_dw_Table1.txt",
+  skip = header_start - 2,
+)
