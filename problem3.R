9d78a817db7a8543a99abdefcc1b590814fa0503
joakim-klemetsen <joakim.klemetsen@icloud.com>
2023-10-05 15:12:14 +0200

    Created a function that creates histograms
    The function checks if variable exists in the data frame and only plots the data if the variable is of type integer or numeric.

--- problem3.R
+++ problem3.R
@@ -1,1 +1,37 @@
+# Problem 3 - Git and Github Assignment
+
+# Load required packages ---
+
+# ---
+
+# Create a function that plots histograms ---
+plotVariable <- function(data, variable) {
+  
+  # Check if variable is in data and is numeric
+  if(variable %in% names(data) && is.numeric(data[[variable]])) {
+    
+    # Plot
+    p <- ggplot(data, aes(x = .data[[variable]])) +
+      geom_histogram(
+        fill = "skyblue", 
+        color = "black", 
+        alpha = 0.7
+      ) +
+      labs(
+        title = paste("Histogram of ", variable),
+        x = variable,
+        y = "Frequency"
+      ) +
+      theme_minimal()
+    return(p)
+    
+    # If the specified variable is invalid or not numeric   
+  } else {
+    print(paste("Invalid input or non-numeric variable: ", variable))
+    return(NULL)
+  }
+}
+# ---
+
+
 
